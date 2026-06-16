11c2735438773c8ce85fdcd456a816e3554fdc83
NathanTCR <9019842@student.zadkine.nl>
2026-06-16 13:24:55 +0200

    Update riddles.sql

--- sql/riddles.sql
+++ sql/riddles.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 16, 2026 at 11:16 AM
+-- Generation Time: Jun 16, 2026 at 11:24 AM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.30
 
@@ -32,19 +32,21 @@ CREATE TABLE `riddles` (
   `riddle` varchar(255) NOT NULL,
   `answer` varchar(100) NOT NULL,
   `hint` varchar(255) DEFAULT NULL,
-  `roomId` int NOT NULL
+  `roomId` int NOT NULL,
+  `timelimit(seconds)` int NOT NULL,
+  `team` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `riddles`
 --
 
-INSERT INTO `riddles` (`id`, `riddle`, `answer`, `hint`, `roomId`) VALUES
-(1, 'Finish the sentence: \"Force equals mass times...\"', 'Acceleration', 'Something speeding up', 2),
-(3, 'What would element 79 be', 'Gold', 'It\'s a type of metal', 2),
-(4, 'What\'s the furthest planet from the sun?', 'Neptune', NULL, 1),
-(5, 'What\'s the name of George Orwell\'s dystopian novel about mass survaillance?', '1984', NULL, 1),
-(6, 'Finish the title: \"Dirty Deeds...', 'Dirty Deeds Done Dirt Cheap', NULL, 1);
+INSERT INTO `riddles` (`id`, `riddle`, `answer`, `hint`, `roomId`, `timelimit(seconds)`, `team`) VALUES
+(1, 'Finish the sentence: \"Force equals mass times...\"', 'Acceleration', 'Something speeding up', 2, 0, ''),
+(3, 'What would element 79 be', 'Gold', 'It\'s a type of metal', 2, 0, ''),
+(4, 'What\'s the furthest planet from the sun?', 'Neptune', NULL, 1, 0, ''),
+(5, 'What\'s the name of George Orwell\'s dystopian novel about mass survaillance?', '1984', NULL, 1, 0, ''),
+(6, 'Finish the title: \"Dirty Deeds...', 'Dirty Deeds Done Dirt Cheap', NULL, 1, 0, '');
 
 --
 -- Indexes for dumped tables
